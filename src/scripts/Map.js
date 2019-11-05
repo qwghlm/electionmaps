8d373d532ccfd885a13a30f6456569d92ef56c11
Chris Applegate <github@qwghlm.co.uk>
2019-11-05 17:36:56 +0100

    Create an SVG

---
+++ src/scripts/Map.js
@@ -0,0 +1,26 @@
+import d3 from "./lib/d3";
+
+export default class Map {
+
+  constructor(el, config) {
+    const { aspectRatio } = config;
+    this.initSVG(el, aspectRatio);
+  }
+
+  initSVG(el, aspectRatio) {
+
+    this.width = (el.offsetWidth === 0) ? el.parentNode.offsetWidth : el.offsetWidth;
+    this.height = aspectRatio*this.width;
+
+    this.$svg = d3.select(el).append("svg")
+      .attr("xmlns", "http://www.w3.org/2000/svg")
+      .attr("width", this.width)
+      .attr("height", this.height)
+      .attr("style", `height: ${this.height} px !important;`);
+
+    this.$map = this.$svg;
+
+  }
+}
+
+// TODO eslint etc.
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,10 @@
 import '../styles/index.scss';
+
+import Map from "./Map";
+
+document.addEventListener("DOMContentLoaded", function(e) {
+  const el = document.querySelector("#map-wrapper");
+  return new Map(el, {
+    aspectRatio: 2,
+  });
+});
